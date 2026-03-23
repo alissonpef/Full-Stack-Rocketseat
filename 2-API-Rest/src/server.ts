c04454a8f470c10d52bdd690a5f6f5b736863ad1
Alisson <alissonpef@gmail.com>
2026-03-23 16:20:54 -0300

    feat: set up Express server with initial route

--- 2-API-Rest/src/server.ts
+++ 2-API-Rest/src/server.ts
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+import express from "express";
+
+const PORT = 3333;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World Express");
+});
+
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
