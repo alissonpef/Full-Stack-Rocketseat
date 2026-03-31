33739d32b43c7451a1e8ce275aef8d73699af621
Alisson <alissonpef@gmail.com>
2026-03-31 11:00:25 -0300

    feat: fix product route endpoint and enhance post response

--- 2-API-Rest/src/server.ts
+++ 2-API-Rest/src/server.ts
@@ -1,13 +1,22 @@
 import express from "express";
 
 const PORT = 3333;
-const app = express();
 
-app.get("/product/:id", (req, res) => {
+const app = express();
+app.use(express.json())
+
+app.get("/products/:id", (req, res) => {
   const { id } = req.params;
   const { page, limit } = req.query;
 
   res.send(`Produto ${id}, página ${page} de ${limit}`);
 });
+
+app.post("/products", (req, res) => {
+  const { name, price } = req.body;
+
+  // res.send(`Produto ${name} custa ${price}`)
+  res.status(201).json({ name, price })
+})
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
