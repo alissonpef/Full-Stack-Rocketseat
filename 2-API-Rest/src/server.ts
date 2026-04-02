bfef3e048bc2249cdd8daa1a260e672197c00af8
Alisson <alissonpef@gmail.com>
2026-04-02 09:25:33 -0300

    feat: add middleware to product post route and clean up code formatting

--- 2-API-Rest/src/server.ts
+++ 2-API-Rest/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import { myMiddleware } from "./middleware/my-middleware";
 
 const PORT = 3333;
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+// app.use(myMiddleware); Middleware Global
 
 app.get("/products/:id", (req, res) => {
   const { id } = req.params;
@@ -12,11 +14,11 @@ app.get("/products/:id", (req, res) => {
   res.send(`Produto ${id}, página ${page} de ${limit}`);
 });
 
-app.post("/products", (req, res) => {
+app.post("/products", myMiddleware, (req, res) => {
   const { name, price } = req.body;
 
   // res.send(`Produto ${name} custa ${price}`)
-  res.status(201).json({ name, price })
-})
+  res.status(201).json({ name, price, user_id: req.user_id });
+});
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
