010c5c3861fe63bb4b2b25da1ce09df4fb6128b2
alissonpef <alissonpef@gmail.com>
2026-04-20 10:39:47 -0300

    refactor: replace inline product routes with modular routes from routes file

--- 2-API-Rest/src/server.ts
+++ 2-API-Rest/src/server.ts
@@ -1,24 +1,11 @@
 import express from "express";
-import { myMiddleware } from "./middleware/my-middleware";
+import { routes } from "./routes"
 
 const PORT = 3333;
 
 const app = express();
 app.use(express.json());
-// app.use(myMiddleware); Middleware Global
 
-app.get("/products/:id", (req, res) => {
-  const { id } = req.params; // URL
-  const { page, limit } = req.query; // ?...
-
-  res.send(`Produto ${id}, página ${page} de ${limit}`);
-});
-
-app.post("/products", myMiddleware, (req, res) => {
-  const { name, price } = req.body;
-
-  // res.send(`Produto ${name} custa ${price}`)
-  res.status(201).json({ name, price, user_id: req.user_id });
-});
+app.use(routes)
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
