c98e8bb5f0b3143be26bad5d86be008ca5e789ca
alissonpef <alissonpef@gmail.com>
2026-04-20 10:39:57 -0300

    feat: add products routes for handling GET and POST requests

---
+++ 2-API-Rest/src/routes/products-routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express"
+import { myMiddleware } from "../middleware/my-middleware";
+
+const productsRoutes = Router()
+
+productsRoutes.get("/:id", (req, res) => {
+  const { id } = req.params; // URL
+  const { page, limit } = req.query;
+
+  res.send(`Produto ${id}, página ${page} de ${limit}`);
+});
+
+productsRoutes.post("/", myMiddleware, (req, res) => {
+  const { name, price } = req.body;
+
+  // res.send(`Produto ${name} custa ${price}`)
+  res.status(201).json({ name, price, user_id: req.user_id });
+});
+
+export { productsRoutes }
